9c69ed4966245690547edea0e1f1171c3045c993
John Le Drew <jp@antz29.com>
2012-08-27 10:33:37 +0100

    Adjust bootstrapper for Composer

--- src/load.php
+++ src/load.php
@@ -1,25 +1,26 @@
 <?php
 if (!defined('TAPPED_CACHE')) define("TAPPED_CACHE",5);
 
-define('ROOT',realpath(dirname(dirname($_SERVER['SCRIPT_FILENAME']))) . '/');
-define('PUBLIC_ROOT',realpath(dirname($_SERVER['SCRIPT_FILENAME'])) . '/');
+define('ROOT',realpath(dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME'])))) . '/');
+define('APP_ROOT', ROOT . 'src/');
+define('PUBLIC_ROOT',APP_ROOT . 'public/');
 
-define("APP_ROOT",realpath(dirname(dirname(dirname(__FILE__)))).'/');
-define("BASE_ROOT",realpath(APP_ROOT.'vendor/base').'/');
-define("CONFIG_ROOT",realpath(APP_ROOT . 'config').'/');
-define("TEMPLATE_ROOT",realpath(APP_ROOT.'templates').'/');
+define("BASE_ROOT",realpath(ROOT . 'vendor/antz29/base/src') . '/');
+
+define("CONFIG_ROOT",realpath(APP_ROOT . 'config') . '/');
+define("TEMPLATE_ROOT",realpath(APP_ROOT .'templates') . '/');
 define("DS",DIRECTORY_SEPARATOR);
 
-include BASE_ROOT."tapped/tapped.php";
+include BASE_ROOT . "tapped/tapped.php";
 
 $tapped = Base\Tapped::getInstance();
 $tapped->setCache(TAPPED_CACHE);
 
+$tapped->addPath(ROOT."vendor");
 $tapped->addPath(APP_ROOT."lib");
 $tapped->addPath(APP_ROOT."controllers");
 $tapped->addPath(APP_ROOT."models");
 $tapped->addPath(APP_ROOT."resources");
-$tapped->addPath(APP_ROOT."vendor");
 $tapped->addPath(APP_ROOT."modules");
 
 $tapped->registerAutoloader();
